55a4795b82d172db2b608c92ca9c3dec98aa3840
Pierre Joubert <joubertpierre96@gmail.com>
2024-08-01 00:50:42 +0200

    Cloned repository

--- To-Do List/obj/Debug/net8.0-windows/To-Do List.AssemblyInfo.cs
+++ To-Do List/obj/Debug/net8.0-windows/To-Do List.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("To-Do List")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a71af62ecef6c32aa6680d0e08a0748580fa3c3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31b5e4a098246d41b3bab819006d95156e8d42d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("To-Do List")]
 [assembly: System.Reflection.AssemblyTitleAttribute("To-Do List")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
