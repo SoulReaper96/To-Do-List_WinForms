41764f11a194e6b2e5a76af651edd74ccfd3072c
Pierre Joubert <joubertpierre96@gmail.com>
2024-07-31 23:16:11 +0200

    Repository clone to local machine.

--- To-Do List/obj/Release/net8.0-windows/To-Do List.AssemblyInfo.cs
+++ To-Do List/obj/Release/net8.0-windows/To-Do List.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("To-Do List")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a71af62ecef6c32aa6680d0e08a0748580fa3c3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("To-Do List")]
